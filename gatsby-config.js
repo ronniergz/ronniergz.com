98877d65b5862c8f20bbd5a3badc099d49618edd
Ronnie <rlrodrig88@gmail.com>
2021-03-03 08:51:57 -0600

    added gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -90,6 +90,16 @@ module.exports = {
         trackingId: process.env.GA_TRACKING_ID,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [
+          process.env.GA_TRACKING_ID, // Google Analytics / GA
+          "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+      },
+    },
 
   ],
 }
